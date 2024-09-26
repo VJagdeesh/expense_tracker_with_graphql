e03887cbc1fb9e083ac8232b982ad15d12394d4b
unknown <jagdeeshbhavani22@gmail.com>
2024-09-26 23:09:30 +0530

    feat: passport and mongo db configured

---
+++ Backend/resolvers/user.resolver.js
@@ -0,0 +1,14 @@
+import { users } from "../dummydata/data.js";
+const userResolver = {
+  Query: {
+    users: (_, _, { req, res }) => {
+      return users;
+    },
+    user: (_, { userId }) => {
+      return users.find((user) => user._id === userId);
+    },
+  },
+  Mutation: {},
+};
+
+export default userResolver;
